0057a7094f64b8b8d0eff780127e1589a47f7eba
marcusstenbeck <marcus.stenbeck@gmail.com>
2011-02-28 15:37:14 +0100

    Skitit i avancerade binärträd

--- GameObject.cpp
+++ GameObject.cpp
@@ -13,14 +13,20 @@
 using namespace Ogre;
 using namespace fury;
 
-void GameObjectRegistry::add(RigidBody *rb, Ogre::SceneNode *sn)
+void GameObjectRegistry::add(RigidBody *rb, Ogre::SceneNode *sn, std::string gName)
 {
 	GameObjectRegistration gor;
 	
 	gor.sn = sn;
 	gor.rb = rb;
+	gor.gName = gName;
 	
 	registrations.push_back(gor);
+	
+	// Om det inte redan finns en toppnod
+	// i boundingvolumeheirarkin så skapar vi toppnoden
+	
+	
 };
 
 void GameObjectRegistry::remove(RigidBody *rb, Ogre::SceneNode *sn)
@@ -47,5 +53,51 @@ void GameObjectRegistry::updateSceneNodes(real duration)
 		i->rb->integrate(duration);
 		i->sn->_setDerivedPosition(i->rb->position);
 		i->sn->setOrientation(i->rb->orientation);
+	}
+};
+
+struct GameObjectRegistry::GameObjectRegistration* GameObjectRegistry::getGameObjectRegistration(std::string s)
+{
+	for (Registry::iterator i = registrations.begin(); i != registrations.end(); ++i)
+	{
+		if (i->gName == s) 
+		{
+			return &*i;
+		}
+	}
+	
+	return NULL;
+};
+
+void GameObjectRegistry::runCollisions()
+{
+	std::vector<PotentialContact> pcr;
+	
+	for (Registry::iterator i = registrations.begin(); i != registrations.end(); ++i)
+	{
+		// testa en registration mot alla andra registrations
+		BoundingSphere* thisBs = new BoundingSphere(i->rb->position, .7);
+		
+		for (Registry::iterator j = registrations.begin(); j != registrations.end(); ++j)
+		{
+			if (j != i)
+			{
+				BoundingSphere* thatBs = new BoundingSphere(j->rb->position, .7);
+				
+				if(thisBs->overlaps(thatBs))
+				{
+					PotentialContact pc;
+					pc.body[0] = i->rb;
+					pc.body[1] = j->rb;
+					
+					pcr.push_back(pc);
+					std::cout << "Overlap!" << std::endl;
+				}
+				
+				delete thatBs;
+			}
+		}
+		
+		delete thisBs;
 	}
 };
--- collide_coarse.cpp
+++ collide_coarse.cpp
@@ -12,8 +12,64 @@
 
 using namespace fury;
 
+BoundingSphere::BoundingSphere(const Ogre::Vector3 &center, real radius)
+{
+    BoundingSphere::center = center;
+    BoundingSphere::radius = radius;
+}
+
 bool BoundingSphere::overlaps(const BoundingSphere *other) const
 {
 	real distanceSquared = (center - other->center).squaredLength();
 	return distanceSquared < (radius + other->radius)* (radius + other->radius);
+}
+
+BoundingSphere::BoundingSphere(const BoundingSphere &one,
+                               const BoundingSphere &two)
+{
+	Ogre::Vector3 centerOffset = two.center - one.center;
+    real distance = centerOffset.squaredLength();
+    real radiusDiff = two.radius - one.radius;
+	
+    // Check if the larger sphere encloses the small one
+    if (radiusDiff*radiusDiff >= distance)
+    {
+        if (one.radius > two.radius)
+        {
+            center = one.center;
+            radius = one.radius;
+        }
+        else
+        {
+            center = two.center;
+            radius = two.radius;
+        }
+    }
+	
+    // Otherwise we need to work with partially
+    // overlapping spheres
+    else
+    {
+        distance = Ogre::Math::Sqrt(distance);
+        radius = (distance + one.radius + two.radius) * ((real)0.5);
+		
+        // The new centre is based on one's centre, moved towards
+        // two's centre by an ammount proportional to the spheres'
+        // radii.
+        center = one.center;
+        if (distance > 0)
+        {
+            center += centerOffset * ((radius - one.radius)/distance);
+        }
+    }	
+}
+
+
+real BoundingSphere::getGrowth(const BoundingSphere &other) const
+{
+    BoundingSphere newSphere(*this, other);
+	
+    // We return a value proportional to the change in surface
+    // area of the sphere.
+    return newSphere.radius*newSphere.radius - radius*radius;
 }
